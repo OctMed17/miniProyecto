2a8eef891e9c331f53c98bcbea87089cbea7a897
Octavio Alejandro Medina Corvera <octavio.medina@neoris.cxnetworks.net>
2018-06-22 15:56:58 -0600

    se crea el proyecto en blanco agregando modulos de bootstrap y fontawesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StudentComponent } from './student/student.component';
+import { TeacherComponent } from './teacher/teacher.component';
+import { LoginComponent } from './login/login.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { AvisosComponent } from './avisos/avisos.component';
+import {RouterModule, Routes} from'@angular/router';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+const appRoutes : Routes=[
+  {path:'', component: LoginComponent},
+  {path:'principal', component: PrincipalComponent},
+  {path:'student',component:StudentComponent},
+  {path:'avisos/:id',component:AvisosComponent},
+  {path:'teacher', component:TeacherComponent},
+  {path:'login',component:LoginComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentComponent,
+    TeacherComponent,
+    LoginComponent,
+    PrincipalComponent,
+    AvisosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
